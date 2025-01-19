74142c791e87f6fc3a3db34353147f484fe11060
Fivvee0 <167813307+Fivvee0@users.noreply.github.com>
2025-01-19 16:12:37 +0100

    little fix

--- include/string.h
+++ include/string.h
@@ -3,6 +3,7 @@
 #include <types.h>
 
 extern int strlen(const char *str);
+extern char *joinstr(const char *str1, const char *str2, char *dest);
 extern void strcpy(char *dest, const char *src);
 extern int streql(const char *str1, const char *str2);
 extern int split_str(char *str, char separator, char **result, int max_splits);
